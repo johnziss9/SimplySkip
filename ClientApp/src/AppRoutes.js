7a0d421c20364b57a75f76443aca5b74532e54b5
John Z <john.zissimou@and.digital>
2023-11-06 10:46:46 +0000

    Added the Reminders page and the route for it.

--- ClientApp/src/AppRoutes.js
+++ ClientApp/src/AppRoutes.js
@@ -1,10 +1,15 @@
 import Home from "./pages/Home/Home";
+import Reminders from "./pages/Reminders/Reminders";
 
 const AppRoutes = [
   {
     index: true,
     element: <Home />
-  }
+  },
+  {
+    path: '/Reminders',
+    element: <Reminders />
+  },
 ];
 
 export default AppRoutes;
---
+++ ClientApp/src/pages/Reminders/Reminders.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import './Reminders.css';
+
+function Reminders() {
+    return (
+        <>
+            <h1>REMINDERS PAGE</h1>
+        </>
+    )
+}
+
+export default Reminders;
