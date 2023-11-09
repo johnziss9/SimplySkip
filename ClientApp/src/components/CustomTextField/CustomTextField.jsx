45dbffb9abb1aea0c400c12ceacef91dd5b9a5ec
John Z <john.zissimou@and.digital>
2023-11-09 22:35:22 +0000

    Changed the props and css in the CustomTextfield to accomodate the TextField as a Sarch box later.

--- ClientApp/src/components/CustomTextField/CustomTextField.jsx
+++ ClientApp/src/components/CustomTextField/CustomTextField.jsx
@@ -1,29 +1,43 @@
 import React from "react";
 import { TextField } from "@mui/material";
 
-
 function CustomTextField(props) {
-
     return (
         <TextField
             label={props.label}
-            variant="outlined"
+            variant={props.variant}
             margin={props.margin}
             type={props.type}
             onChange={props.onChange}
             value={props.value}
             sx={{
+                // Styles for the outlined variant
                 "& .MuiOutlinedInput-root": {
                     "& fieldset": {
                         borderColor: "#006d77", // Changes border style when not focused
                         borderRadius: 0,
                     },
-                        '&:hover fieldset': {
-                          border: "2px solid" // Changes border style on focus
-                        },
-                        '&.Mui-focused fieldset': {
-                          borderColor: '#006d77', // Changes border style when focused
-                        },
+                    '&:hover fieldset': {
+                        border: "2px solid #006d77", // Changes border style on hover
+                    },
+                    '&.Mui-focused fieldset': {
+                        borderColor: '#006d77 !important', // Changes border style when focused
+                    },
+                },
+                // Styles for the standard variant
+                '& .MuiInputBase-root': {
+                    '&::before': {
+                        borderBottom: '1px solid #006d77', // Change border style when not focused
+                    },
+                    '&:hover::before': {
+                        borderBottom: '2px solid #006d77', // Change border style on hover
+                    },
+                    '&.Mui-focused::before': {
+                        borderBottom: '2px solid #006d77', // Change border style when focused
+                    },
+                    '&::after': {
+                        borderBottom: 'none', // Removed 2px blue border-bottom 
+                    },
                 }
             }}
             InputProps={{
--- ClientApp/src/pages/Home/Home.jsx
+++ ClientApp/src/pages/Home/Home.jsx
@@ -48,8 +48,8 @@ function Home() {
             <div className='home-container'>
                 <div className='home-login-section'>
                     <h1 className='home-title'>Simply Skip</h1>
-                    <CustomTextField label={"Username"} margin={"dense"} onChange={e => setUsername(e.target.value)} value={username} />
-                    <CustomTextField label={"Password"} margin={"dense"} type={"password"} onChange={e => setPassword(e.target.value)} value={password} />
+                    <CustomTextField label={"Username"} variant={"outlined"} margin={"dense"} onChange={e => setUsername(e.target.value)} value={username} />
+                    <CustomTextField label={"Password"} variant={"outlined"} margin={"dense"} type={"password"} onChange={e => setPassword(e.target.value)} value={password} />
                     <CustomButton backgroundColor={"#006d77"} buttonName={"Login"} width={"200px"} height={"50px"} marginTop={"20px"} onClick={handleLogin} />
                 </div>
             </div>
