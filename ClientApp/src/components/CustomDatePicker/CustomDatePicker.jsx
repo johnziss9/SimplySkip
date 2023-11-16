7ffd8ddd3677ff153a8accdc2d40d2cc088dcefa
John Z <john.zissimou@and.digital>
2023-11-16 10:30:19 +0000

    Created a Datepicker component.

---
+++ ClientApp/src/components/CustomDatePicker/CustomDatePicker.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { PickersDay } from '@mui/x-date-pickers/PickersDay';
+import { styled } from '@mui/material/styles';
+
+function CustomDatePicker(props) {
+
+    const StyledDay = styled(PickersDay)(({ theme }) => ({
+        borderRadius: theme.shape.borderRadius,
+        color: '#006d77'
+    }));
+
+    return (
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+                label="Styled picker"
+                slots={{
+                    day: StyledDay
+                }}
+                sx={{
+                    "& .MuiInputLabel-root.Mui-focused": { color: "#006d77" }, // Change the label colour on focus
+                    "& .MuiOutlinedInput-root": {
+                        "& fieldset": {
+                            borderColor: "#006d77", // Changes border style when not focused
+                            borderRadius: 0,
+                        },
+                        '&:hover fieldset': {
+                            border: "2px solid #006d77", // Changes border style on hover
+                        },
+                        '&.Mui-focused fieldset': {
+                            borderColor: '#006d77 !important', // Changes border style when focused
+                        },
+                    },
+                    input: { color: '#006d77' }, // Changes the colour of the font inside the datepicker
+                    svg: { color: '#006d77' }, // Changes the colour of the calendar icon
+                    label: { color: '#006d77' } // Changes the colour of the calendar icon
+                }}
+            />
+        </LocalizationProvider>
+    );
+}
+
+export default CustomDatePicker;
